40d3e5cd04c6a74ee5c08e5353a37ddb821428e0
lshoyfer <lshoyfer@gmail.com>
2024-02-27 20:22:17 -0500

    Change libs & dependencies

--- gamename_binary/src/main.rs
+++ gamename_binary/src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+use ggez::{
+    event,
+    // glam,
+    Context,
+    GameResult,
+};
+
+struct MainState;
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
+fn main() -> GameResult {
+    // todo()! resources later
+
+    let cb = ggez::ContextBuilder::new("gamename", "lshoyfer");
+    let (ctx, event_loop) = cb.build()?;
+    let state = MainState;
+    event::run(ctx, event_loop, state)
+}
